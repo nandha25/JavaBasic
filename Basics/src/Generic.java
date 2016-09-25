5bc065c3b09e96dfe11a0e651935d0cf1a4c6998
Nandhakumar Chellamuthu <nandhakumar.tc@gmail.com>
2016-09-25 12:11:00 +0530

    Initial commit

---
+++ Basics/src/Generic.java
@@ -0,0 +1,9 @@
+/**
+ * Created by nandh on 25-09-2016.
+ */
+public class Generic {
+
+    public  static void main (String args[]){
+
+    }
+}
---
+++ Basics/src/GenericClass.java
@@ -0,0 +1,5 @@
+/**
+ * Created by nandh on 25-09-2016.
+ */
+public class GenericClass {
+}
